430692db0cdcbe30a222638c96488bf488119beb
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2025-10-15 17:37:41 +0100

    feat: update navigation links in signin and signup components to use routerLink

--- src/app/signin/signin.ts
+++ src/app/signin/signin.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-signin',
-    imports: [ReactiveFormsModule, FormsModule],
+    imports: [ReactiveFormsModule, FormsModule, RouterLink],
     templateUrl: './signin.html',
     styleUrl: './signin.css'
 })
--- src/app/signup/signup.ts
+++ src/app/signup/signup.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-signup',
-    imports: [FormsModule, ReactiveFormsModule],
+    imports: [FormsModule, ReactiveFormsModule, RouterLink],
     templateUrl: './signup.html',
     styleUrl: './signup.css'
 })
