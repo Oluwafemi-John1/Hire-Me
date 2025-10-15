caa362ad03b38a55114cfdda3d7cd451ed4b8324
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2025-10-15 14:00:06 +0100

    feat: add homepage component with routing and basic structure; update signin link in signup form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { Signup } from './signup/signup';
+import { Signin } from './signin/signin';
+import { Homepage } from './homepage/homepage';
 
 export const routes: Routes = [
-    {path: 'signup', component: Signup}
+    {path: 'signup', component: Signup},
+    {path: 'signin', component: Signin},
+    {path: 'home', component: Homepage}
 ];
--- src/app/signin/signin.ts
+++ src/app/signin/signin.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-signin',
-  imports: [],
-  templateUrl: './signin.html',
-  styleUrl: './signin.css'
+    selector: 'app-signin',
+    imports: [ReactiveFormsModule, FormsModule],
+    templateUrl: './signin.html',
+    styleUrl: './signin.css'
 })
 export class Signin {
+    private builder = inject(FormBuilder)
+
+    signInForm = this.builder.group({
+        email: ["", [Validators.required, Validators.email]],
+        passWord: ["", [Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)]]
+    })
+
+    logIn() {
+        console.log(this.signInForm);
+    }
 
 }
