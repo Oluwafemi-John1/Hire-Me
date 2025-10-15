19ee21e9b9f4a10ab15d7e668fea6bde2d6be947
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2025-10-15 09:06:38 +0100

    feat: add signup component and routing
    - Created a new Signup component with its HTML, CSS, and spec files.
    - Updated app routing to include a path for the Signup component.
    - Modified app component template to remove placeholder content and include a router outlet.
    - Added Bootstrap CSS and JS links to index.html for styling.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// import { Signup } from "./signup/signup";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.html',
+    styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('hireme');
+    protected readonly title = signal('hireme');
 }
