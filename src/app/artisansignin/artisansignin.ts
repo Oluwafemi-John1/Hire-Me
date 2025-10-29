36d24065be530d86128b47ca030fbcdb1c1fe119
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2025-10-29 23:42:06 +0100

    fix: implement HTTP request for sign-in and handle response errors

--- src/app/artisansignin/artisansignin.ts
+++ src/app/artisansignin/artisansignin.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import * as AOS from 'aos';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'app-artisansignin',
@@ -12,6 +13,8 @@ import * as AOS from 'aos';
 })
 export class Artisansignin implements OnInit {
     private builder = inject(FormBuilder)
+    private http = inject(HttpClient)
+    private router = inject(Router)
     errorMessage: string = ''
     showError: boolean = false
 
@@ -32,14 +35,23 @@ export class Artisansignin implements OnInit {
         this.showError = false;
         this.errorMessage = '';
 
-        console.log(this.signInForm);
-
-        // Simulate login validation
-        if (this.signInForm.valid) {
-            // Add your login logic here
-            // Example error handling:
-            // this.errorMessage = 'Invalid email or password. Please try again.';
-            // this.showError = true;
-        }
+        this.http.post('http://localhost:8888/HireMe/ArtisanAuth.php', this.signInForm.value)
+            .subscribe({
+                next: (response: any) => {
+                    console.log(response)
+                    if (response.status === 200) {
+                        console.log('I will go to Dashboard');
+                        this.router.navigate(['/artisandashboard'])
+                    } else {
+                        this.errorMessage = response.message || 'Sign in failed. Please try again.';
+                        this.showError = true;
+                    }
+                },
+                error: (error) => {
+                    this.errorMessage = 'An error occurred. Please check your connection and try again.';
+                    this.showError = true;
+                    console.error('Error:', error);
+                }
+            })
     }
 }
