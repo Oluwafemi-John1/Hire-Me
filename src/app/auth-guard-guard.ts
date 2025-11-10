b81c59735a34b6cbc2238db4d893f90b505c0bb1
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2025-11-10 17:46:10 +0100

    fix: clean up unused HttpClient import and variable in auth guard

--- src/app/auth-guard-guard.ts
+++ src/app/auth-guard-guard.ts
@@ -1,24 +1,25 @@
-import { HttpClient } from '@angular/common/http';
+// import { HttpClient } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { jwtDecode, JwtPayload } from 'jwt-decode';
 
 export const authGuardGuard: CanActivateFn = (route, state) => {
     const token = localStorage['token'];
-    const _http = inject(HttpClient);
-    const _router = inject(Router);
+    // const _http = inject(HttpClient);
+    const router = inject(Router);
 
     if(token) {
         const decoded:any = jwtDecode<JwtPayload>(token);
         const expTime = decoded.exp * 1000;
         const now = Date.now()
         if((now - expTime) < 3600) {
-            return true
+            return true;
         } else {
-            _router.navigate(['/signin'])
+            router.navigate(['/signin'])
+            return false;
         }
     }
 
-    _router.navigate(['/signin'])
+    router.navigate(['/signin'])
     return false;
 };
